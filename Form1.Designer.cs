ae035e64cde5986d18a2002742b693f86000d608
Илья Тупицин <ilhatupitsin@yandex.ru>
2023-11-28 13:17:06 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace Учебная_практика
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Honeydew;
+            this.button1.Font = new System.Drawing.Font("Calisto MT", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(753, 438);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "сравнить";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightCyan;
+            this.button2.Font = new System.Drawing.Font("Calisto MT", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(916, 438);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 54);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "сохранить";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedHeaders;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.Color.Black;
+            this.dataGridView1.Location = new System.Drawing.Point(261, 75);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(5);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(787, 27);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView2.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedHeaders;
+            this.dataGridView2.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(261, 146);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(787, 27);
+            this.dataGridView2.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Info;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 211);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1036, 221);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 75);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(243, 26);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(12, 146);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(243, 26);
+            this.comboBox2.TabIndex = 6;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(72, 451);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 25);
+            this.label1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(274, 451);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 25);
+            this.label2.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Californian FB", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(317, 27);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Выберите фильмы для сравнения";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(311, 54);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(130, 18);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "актерский состав";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(505, 54);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 18);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "режиссура";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(681, 54);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(183, 18);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "маркетинговая кампания";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(896, 54);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(152, 18);
+            this.label7.TabIndex = 13;
+            this.label7.Text = " актуальность жанра";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(1060, 521);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Californian FB", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "анализ фильмов";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.LinkLabel;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Учебная_практика
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            PopulateComboBox1();
+            PopulateComboBox2();
+        }
+        private void PopulateComboBox1()
+        {
+            string folderPath = @"C:\Users\Пользователь\Desktop\фильмы";
+            string[] fileEntries = Directory.GetFiles(folderPath);
+            foreach (string fileName in fileEntries)
+            {
+                comboBox1.Items.Add(Path.GetFileName(fileName));
+
+            }
+        }
+        private void PopulateComboBox2()
+        {
+            string folderPath = @"C:\Users\Пользователь\Desktop\фильмы";
+            string[] fileEntries = Directory.GetFiles(folderPath);
+            foreach (string fileName in fileEntries)
+            {
+                comboBox2.Items.Add(Path.GetFileName(fileName));
+
+            }
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            string selectedFileName = comboBox1.SelectedItem.ToString();
+            string filePath = Path.Combine("C:\\Users\\Пользователь\\Desktop\\фильмы", selectedFileName);
+            DataTable dt = new DataTable();
+
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                string line = sr.ReadLine();
+                string[] values = line.Split(',');
+
+                // Проходим по всем значениям, чтобы сформировать уникальные имена столбцов
+                Dictionary<string, int> columnCounts = new Dictionary<string, int>();
+                foreach (string header in values)
+                {
+                    string columnName = header;
+                    if (columnCounts.ContainsKey(columnName))
+                    {
+                        columnCounts[columnName]++;
+                        columnName = $"{columnName} ({columnCounts[columnName]})";
+                    }
+                    else
+                    {
+                        columnCounts.Add(columnName, 1);
+                    }
+                    // Добавляем столбец с уникальным именем
+                    dt.Columns.Add(columnName);
+
+                }
+            }
+
+            dataGridView1.DataSource = dt;
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedFileName = comboBox2.SelectedItem.ToString();
+            string filePath = Path.Combine("C:\\Users\\Пользователь\\Desktop\\фильмы", selectedFileName);
+            DataTable dt2 = new DataTable();
+
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                string line = sr.ReadLine();
+                string[] values = line.Split(',');
+
+                // Проходим по всем значениям, чтобы сформировать уникальные имена столбцов
+                Dictionary<string, int> columnCounts = new Dictionary<string, int>();
+                foreach (string header in values)
+                {
+                    string columnName = header;
+                    if (columnCounts.ContainsKey(columnName))
+                    {
+                        columnCounts[columnName]++;
+                        columnName = $"{columnName} ({columnCounts[columnName]})";
+                    }
+                    else
+                    {
+                        columnCounts.Add(columnName, 1);
+                    }
+                    // Добавляем столбец с уникальным именем
+                    dt2.Columns.Add(columnName);
+                }
+            }
+
+            dataGridView2.DataSource = dt2;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string selectedFileName = comboBox1.SelectedItem.ToString();
+            string filePath = Path.Combine("C:\\Users\\Пользователь\\Desktop\\фильмы", selectedFileName);
+
+            if (File.Exists(filePath))
+            {
+                string content = File.ReadAllText(filePath); // Чтение содержимого файла
+                string[] numbers = content.Split(','); // Разделение содержимого по запятым
+                int[] values = new int[numbers.Length];
+
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    if (int.TryParse(numbers[i], out int number))
+                    {
+                        values[i] = number; // Преобразование строк в числа
+                    }
+                }
+                string selectedFileName1 = comboBox2.SelectedItem.ToString();
+                string filePath1 = Path.Combine("C:\\Users\\Пользователь\\Desktop\\фильмы", selectedFileName1);
+
+                if (File.Exists(filePath1))
+                {
+                    string content1 = File.ReadAllText(filePath1); // Чтение содержимого файла
+                    string[] numbers1 = content1.Split(','); // Разделение содержимого по запятым
+                    int[] values1 = new int[numbers1.Length];
+
+                    for (int i = 0; i < numbers1.Length; i++)
+                    {
+                        if (int.TryParse(numbers1[i], out int number1))
+                        {
+                            values1[i] = number1; // Преобразование строк в числа
+                        }
+                    }
+                    DrawChart(values, values1); // Вызов метода для построения диаграммы
+                }
+
+            }
+        }
+
+        private void DrawChart(int[] values, int[] values1)
+        {
+            
+            pictureBox1.Refresh();
+
+            using (Graphics g = pictureBox1.CreateGraphics())
+            {
+                int startX = 70;
+                int startY = pictureBox1.Height - 10;
+                int barWidth = 80;
+                int spacing = 50;
+
+               
+                using (Pen pen = new Pen(Color.Red))
+                {
+                    for (int i = 0; i < values.Length; i++)
+                    {
+                        int barHeight = values[i];
+                        int x = startX + (barWidth + spacing) * i;
+                        int y = startY - barHeight;
+
+                        
+                        g.DrawRectangle(pen, x, y, barWidth, barHeight);
+                    }
+                }
+
+                
+                using (Pen pen1 = new Pen(Color.Blue))
+                {
+                    for (int i = 0; i < values1.Length; i++)
+                    {
+                        int barHeight = values1[i];
+                        int x = startX + (barWidth + spacing) * i;
+                        int y = startY - barHeight;
+
+                        
+                        g.DrawRectangle(pen1, x, y, barWidth, barHeight);
+                    }
+                }
+            }
+
+            
+            label1.ForeColor = Color.Red;
+            label2.ForeColor = Color.Blue;
+            label1.Text = "первый фильм";
+            label2.Text = "второй фильм";
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "JPEG Image|*.jpg|PNG Image|*.png|BMP Image|*.bmp";
+                saveFileDialog.Title = "Save Chart As";
+                saveFileDialog.ShowDialog();
+
+               
+                if (saveFileDialog.FileName != "")
+                {
+                    
+                    Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                    pictureBox1.DrawToBitmap(bmp, pictureBox1.ClientRectangle);
+
+                   
+                    string ext = Path.GetExtension(saveFileDialog.FileName).ToLower();
+
+                    switch (ext)
+                    {
+                        case ".jpg":
+                            bmp.Save(saveFileDialog.FileName, ImageFormat.Jpeg);
+                            break;
+                        case ".bmp":
+                            bmp.Save(saveFileDialog.FileName, ImageFormat.Bmp);
+                            break;
+                        case ".png":
+                            bmp.Save(saveFileDialog.FileName, ImageFormat.Png);
+                            break;
+                        default:
+                           
+                            MessageBox.Show("Unsupported file format", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
+
